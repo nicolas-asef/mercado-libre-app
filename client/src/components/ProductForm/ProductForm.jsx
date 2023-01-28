f20c1a18f18868e637e6b760a623d00639a83c6b
nicolas-asef <nicolasasef1@gmail.com>
2023-01-28 14:42:19 -0300

    form add

--- client/src/components/ProductForm/ProductForm.jsx
+++ client/src/components/ProductForm/ProductForm.jsx
@@ -5,25 +5,29 @@ export default function ProductForm(){
 
     const { register, handleSubmit } = useForm()
 
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return(
         <div>
             <h2>Add Product</h2>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <label>Product Name: </label>
-                    <input type="text" name=""/>
+                    <input type="text" {...register('name')}/>
                 </div>
                 <div>
                     <label>Price: </label>
-                    <input type="text" name=""/>
+                    <input type="text" {...register('price')}/>
                 </div>
                 <div>
                     <label>Description: </label>
-                    <input type="text" name=""/>
+                    <input type="text" {...register('description')}/>
                 </div>
                 <div>
                     <label>Category </label>
-                    <select>
+                    <select {...register('category')}>
                         <option value='select'>Category</option>
                         <option value='electro'>Electro</option>
                         <option value='home'>Home</option>
